27378ac67d9f9ca22b705948b73ee04e676aa87a
natali597 <natalinatali597@gmail.com>
2022-11-16 10:02:24 +0200

    server

---
+++ data/products.js
@@ -0,0 +1,108 @@
+const products = [
+  {
+    _id: "1",
+    name: "Apple Airpods ",
+    image: "/images/airpods.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessy High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 10,
+    raiting: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Apple iPhone 12 ",
+    image: "/images/phone.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessy High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 300,
+    countInStock: 0,
+    raiting: 5,
+    numReviews: 9,
+  },
+  {
+    _id: "3",
+    name: "Dyson vacuum cleaner",
+    image: "/images/dyson.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessy High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 259.99,
+    countInStock: 12,
+    raiting: 4,
+    numReviews: 7,
+  },
+  {
+    _id: "4",
+    name: " iRobot ",
+    image: "/images/irobot-alm.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessy High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 139,
+    countInStock: 5,
+    raiting: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: "5",
+    name: "Bluetooth Speaker ",
+    image: "/images/ramkol.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessy High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 39.99,
+    countInStock: 7,
+    raiting: 4,
+    numReviews: 7,
+  },
+  {
+    _id: "6",
+    name: "Internet Radio",
+    image: "/images/images.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessy High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 50,
+    countInStock: 5,
+    raiting: 3,
+    numReviews: 5,
+  },
+  {
+    _id: "7",
+    name: "Playstation",
+    image: "/images/play.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessy High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 420,
+    countInStock: 6,
+    raiting: 4,
+    numReviews: 6,
+  },
+  {
+    _id: "8",
+    name: " hp Leptop",
+    image: "/images/leptop.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessy High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 510,
+    countInStock: 3,
+    raiting: 5,
+    numReviews: 2,
+  },
+];
+
+module.exports = products;
